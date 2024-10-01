4f597bc90b1d70e7942d1570329806ec9adb377f
unknown <daniel.choi91011@gmail.com>
2024-09-30 17:34:05 -0700

    define game variables/load backdrop image

--- Space_Invaders/script.py
+++ Space_Invaders/script.py
@@ -38,3 +38,30 @@ explosion2_fx.set_volume(0.25)
 
 laser_fx = pygame.mixer.Sound("img/laser.wav")
 laser_fx.set_volume(0.25)
+
+#define game variables
+rows = 5
+cols = 5
+alien_cooldown = 1000 #bullet cooldown in milliseconds
+last_alien_shot = pygame.time.get_ticks()
+countdown = 3
+last_count = pygame.time.get_ticks()
+game_over = 0 #0 is no game over, 1 means player has won, -1 means player has lost
+
+
+#define colors
+red = (255, 0, 0)
+green = (0, 255, 0)
+white = (255, 255, 255)
+
+
+#load image
+bg = pygame.image.load("img/bg.png")
+
+def draw_bg():
+    screen.blit(bg, (0, 0))
+
+#define function for creating text
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
