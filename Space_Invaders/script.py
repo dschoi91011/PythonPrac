624d1675fc6a14b04756f837efdd59365089eb7a
unknown <daniel.choi91011@gmail.com>
2024-10-07 14:22:38 -0700

    create alien class

--- Space_Invaders/script.py
+++ Space_Invaders/script.py
@@ -132,3 +132,21 @@ class Bullets(pygame.sprite.Sprite):
             explosion_fx.play()
             explosion = Explosion(self.rect.centerx, self.rect.centery, 2)
             explosion_group.add(explosion)
+
+#create Aliens class
+class Aliens(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/alien" + str(random.randint(1, 5)) + ".png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.move_counter = 0
+        self.move_direction = 1
+
+    def update(self):
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if abs(self.move_counter) > 75:
+            self.move_direction *= -1
+            self.move_counter *= self.move_direction
+
