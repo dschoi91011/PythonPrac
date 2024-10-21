ed564ca34da719412e536627b703b7dd0e8bb0fc
unknown <daniel.choi91011@gmail.com>
2024-10-21 13:20:56 -0700

    quadralateral setup

--- tinyApps/Art/script.js
+++ tinyApps/Art/script.js
@@ -9,3 +9,13 @@ const generateArt = () => {
     const ctx = canv.getContext("2d");
     display.append(canv);
 };
+
+for (let x = 0; x < 255; x++) {
+    for (let y = 0; y < 255; y++) {
+        if ((x ^ y) % 10) {  // Applying XOR and modulus to generate patterns
+            ctx.fillStyle = `rgb(${(x ^ y) % 210}, ${y}, ${x})`; // Dynamic color
+            ctx.fillRect(x * ((x ^ y) % 10), y * ((x ^ y) % 10), x, y); // Dynamic size and position
+        }
+    }
+};
+
