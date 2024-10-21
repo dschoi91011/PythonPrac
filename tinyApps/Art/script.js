642d1f40bcde0aeb7b06dc6a79c95b9b98ac955a
unknown <daniel.choi91011@gmail.com>
2024-10-21 13:22:05 -0700

    generate art updated

--- tinyApps/Art/script.js
+++ tinyApps/Art/script.js
@@ -3,10 +3,27 @@ const generateArt = () => {
     canv.id = 'art';
     canv.height = '500';
     canv.width = '500';
-    canv.style.border = '5px solid #FF66FF'; // Customize as needed
+    canv.style.border = '5px solid #FF66FF';
     canv.style.background = '#000000';
 
     const ctx = canv.getContext("2d");
+
+    // Generate art based on XOR operations
+    for (let x = 0; x < 255; x++) {
+        for (let y = 0; y < 255; y++) {
+            if ((x ^ y) % 10) {
+                ctx.fillStyle = `rgb(${(x ^ y) % 210}, ${y}, ${x})`;
+                ctx.fillRect(x * ((x ^ y) % 10), y * ((x ^ y) % 10), x, y);
+            }
+        }
+    }
+
+    // Add dynamic text to the canvas
+    ctx.font = "15px Courier New";
+    ctx.fillStyle = '#FF66FF';
+    ctx.fillText("`rgb(${(x ^ y) % 210}, ${y}, ${x})`", 180, 490);
+
+    const options = document.getElementById('options');
     display.append(canv);
 };
 
@@ -18,4 +35,8 @@ for (let x = 0; x < 255; x++) {
         }
     }
 };
+
+ctx.font = "15px Courier New";
+ctx.fillStyle = '#FF66FF';
+ctx.fillText("`rgb(${(x ^ y) % 210}, ${y}, ${x})`", 180, 490);
 
