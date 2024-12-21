a4f1e82434ad79564e62332b525c9b22b4aefe98
unknown <daniel.choi91011@gmail.com>
2024-12-21 15:55:51 -0800

    ctoss comp

--- cointoss.py
+++ cointoss.py
@@ -15,3 +15,25 @@ for experimentNumber in range(20):
         results.append(tossResult)
 
     print(results)
+
+        #Code that checks if there is a streak of 6 heads or tails in a row
+
+    heads = 0
+    tails = 0
+    for i in results:
+        if i == 'H':
+            heads += 1
+            tails = 0
+            if heads == 6:
+                numberOfStreaks += 1
+                
+        else:
+            heads = 0
+            tails += 1
+            if tails == 6:
+                numberOfStreaks += 1
+
+
+    print(f"Number of streaks: {numberOfStreaks}")
+
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
