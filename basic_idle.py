6958edb0cc483b979e678a58c681c016513b3f82
unknown <daniel.choi91011@gmail.com>
2025-01-02 16:41:19 -0800

    additional

--- basic_idle.py
+++ basic_idle.py
@@ -255,7 +255,21 @@ elif len(name) > 10:
 else:
     print("user name created")
   
+######################
 
+prices = [10, 20, 30]
+total = 0
+
+for item in prices:
+    total += item
+     
+print(total)
+
+######################
+for x in range(4):
+    for y in range(3):
+        print(f'({x, y}')
+"""
     
 
 e
