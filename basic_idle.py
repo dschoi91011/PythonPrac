dd32e7af4ac91a09486fe927188e6e0fc064279d
unknown <daniel.choi91011@gmail.com>
2025-01-02 16:40:25 -0800

    more prac

--- basic_idle.py
+++ basic_idle.py
@@ -233,8 +233,29 @@ while True:                                     # or command != "quit":
         print("I don't understand that command.")
         
 """
+"""
+numbers = [3, 4, 4, 5, 5, 5, 6, 8, 10]
+singles = []
+for number in numbers:
+    if number not in singles:
+        singles.append(number)
+print(singles)
 
+coordinates = (1, 2, 3)
+x, y, z = coordinates
+print(x)
+
+
+name = input("user_name: ")
+
+if len(name) < 3:
+    print("user name must be at least 3 characters")    
+elif len(name) > 10:
+    print("user name can be a maximum of 10 characters")    
+else:
+    print("user name created")
   
 
     
 
+e
