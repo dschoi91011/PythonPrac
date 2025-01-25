b8918d9b2b7258ac585738bbbad8b0447aaba40c
unknown <daniel.choi91011@gmail.com>
2025-01-24 23:51:38 -0800

    loop init

--- rps.py
+++ rps.py
@@ -3,3 +3,16 @@ print("ROCK, PAPER, SCISSORS")
 Wins = 0
 Losses = 0
 Ties = 0
+
+while True:   #main loop
+    print("%s Wins, %s Losses, %s Ties" % (Wins, Losses, Ties))   # SAME AS    print(f"{Wins} Wins, {Losses} Losses, {Ties} Ties")
+    while True:   #player input loop
+        print("Enter your move: (r)ock (p)aper (s)cissors or (q)uit")
+        playerChoice = input()
+        if playerChoice == 'q':
+            sys.exit()
+        elif playerChoice == 'r' or playerChoice == 'p' or playerChoice == 's':
+            break
+        else:
+            print("Type r, p, s, or q")
+
