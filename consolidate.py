e48eb6fd44358ae06c7874ec8c6d7d0288d483b3
unknown <daniel.choi91011@gmail.com>
2025-01-31 23:26:47 -0800

    consolidate file init

---
+++ consolidate.py
@@ -0,0 +1,24 @@
+from sys import argv
+
+script, vone, vtwo, vthree = argv
+
+print("The script is called:", script)
+print("The first variable is:", vone)
+print("The second variable is:", vtwo)
+print("The third variable is:", vthree)
+
+
+
+#Run in Terminal command line (not Python).
+#Go to directory in which this script is stored:
+#In this case, it would be D:/Python
+#> python ex13.py var_1 var_2 var_3
+#The variables (var_x) can be whatever
+#You can also change the variable names, e.g.:
+#var1, var2, var3, var4 = argv
+#It will still work the same way
+
+
+#this script will also work if done in notepad
+
+"""
