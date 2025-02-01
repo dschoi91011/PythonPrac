fbcdcf6b43c91ef2fdc29a2b58b9ab3194be7efa
unknown <daniel.choi91011@gmail.com>
2025-01-31 23:27:22 -0800

    cons2

--- consolidate.py
+++ consolidate.py
@@ -22,3 +22,25 @@ print("The third variable is:", vthree)
 #this script will also work if done in notepad
 
 """
+from sys import argv
+
+script, user_name = argv     #d:\python> ex14.py Bass
+prompt = '> '
+
+print(f"Hi {user_name}, I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+""")
+"""
