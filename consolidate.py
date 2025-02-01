781228d14c46ebda0ba00fb66aa4652b3788c609
unknown <daniel.choi91011@gmail.com>
2025-01-31 23:29:17 -0800

    cons5

--- consolidate.py
+++ consolidate.py
@@ -104,3 +104,30 @@ target.write(f"{line1}\n{line2}\n{line3}\n")
 print("And finally, we close it.")
 target.close()
 """
+"""
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print(f"Copying from {from_file} to {to_file}")
+
+in_file = open(from_file)
+indata = in_file.read()     #indata = open(from_file).read() should also work
+
+print(f"The input file is {len(indata)} bytes long")
+
+print(f"Does the output file exist? {exists(to_file)}")
+print("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print("Alright, all done.")
+
+out_file.close()
+in_file.close()     #if you wrote indata = open(from_file).read(),
+                    #get rid of in_file.close(). Python will
+                    #auto-close that line of code once it runs
+"""
