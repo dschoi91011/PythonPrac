b2ca7f07920d8e3e75a33edfca3908a422012f73
unknown <daniel.choi91011@gmail.com>
2025-02-08 01:35:38 -0800

    ex20B

--- consolidate.py
+++ consolidate.py
@@ -204,3 +204,22 @@ def print_a_line(line_count, f):
     #returns the \n that's in the file at the end of that line.
     #add end="" at the end of print function calls to avoid adding
     #double \n to every line
+
+current_file = open(input_file)
+
+print("First let's print the whole file:\n")
+print_all(current_file)
+
+print("\nNow let's rewind, kind of like a tape.")
+rewind(current_file)
+
+print("Let's print three lines:")
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
