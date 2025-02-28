9cab8898a7b7634b8daff5ec2b44da7487ebcf1e
unknown <daniel.choi91011@gmail.com>
2025-02-28 14:59:03 -0800

    ex26_mini

--- ex23plus.py
+++ ex23plus.py
@@ -274,3 +274,27 @@ sorted_words = ex25.sort_sentence(sentence)
 sorted_words
 ex25.print_first_and_last(sentence)
 ex25.print_first_and_last_sorted(sentence)
+
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+from sys import argv
+script, filename = argv
+
+txt = open(filename)
+
+print("Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
