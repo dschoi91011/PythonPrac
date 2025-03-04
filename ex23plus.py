7a0698f8a288f97437b21a80d06e56b175caed6d
unknown <daniel.choi91011@gmail.com>
2025-03-03 17:02:12 -0800

    ex38.2

--- ex23plus.py
+++ ex23plus.py
@@ -548,4 +548,35 @@ print(stuff.pop()) #remove last item
 print(' '.join(stuff)) #joins list item into one string
 print('#'.join(stuff[3:5])) #joins index list items into string
 
-      
+ten_things = "apples oranges crows telephone light sugar"
+print(ten_things)
+
+stuff = ten_things.split(' ')
+print(stuff)
+
+
+more_stuff = ["day", "night", "song", "frisbee", "corn", "banana", "girl", "boy"]
+
+while len(stuff) != 10:
+    print("MORE_STUFF:", more_stuff)
+    next_one = more_stuff.pop()
+    print("adding: ", next_one)
+    stuff.append(next_one)
+    print(f"There are {len(stuff)} items now.")
+
+print("STUFF: ", stuff)
+
+
+
+print("Lets do some things with stuff.\n")
+
+print(stuff[1])
+print(stuff[-1])
+print(stuff.pop())
+print(stuff.pop(3))
+print(stuff)
+print(' '.join(stuff))
+print('#'.join(stuff[3:5]))
+print('-'.join(stuff))
+
+     
