52a52f830367a5b2af2c2a9f405aed56cce9901d
unknown <daniel.choi91011@gmail.com>
2025-03-21 14:39:06 -0700

    newprc

---
+++ newprc.py
@@ -0,0 +1,37 @@
+#message = "hello world"
+#print(message)
+
+#name = "ada lovelace"
+#print(name.title())
+
+#first_name = "ada"
+#last_name = "lovelace"
+#full_name = f"{first_name} {last_name}"
+#print(full_name)
+
+#print(f"Hello, {full_name.title()}!")
+#print("\tPython")
+
+#print("Languages:\n\tEnglish\n\tKorean \n\tSpanish")
+#print("\t\tsecondary bullet point")
+
+#variable = "Eric"
+#print(f"Hello, {variable.title()}! This is a test run.")
+#print("This\n\tis\n\t\talso\n\t\t\tsome\n\t\ttest\n\trun")
+
+#famous_person = "Albert Einstein"
+#message = f"{famous_person} formulated the theory of relativity."
+#print(message)
+
+#famous_person = "Albert Einstein"
+#message = "was a college dropout."
+#total_message = f"{famous_person} {message}"
+#print(total_message)
+
+#print(4*5)
+#integer = 4*5
+#print(f"\t{integer}")
+#favorite_number = 13
+#print(f"My favorite number is: \n{favorite_number}")
+
+#LISTS -------------------------------------------------------------------------------------
